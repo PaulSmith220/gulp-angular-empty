2cba3a4f0259fd2c7954b8edeaa51e1a10114da2
Paul Smith <kuznetps@gmail.com>
2015-02-18 19:46:20 +0500

    left part

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -6,7 +6,6 @@ $(function() {
             full: item.img, // Separate image for the fullscreen mode.
             caption: item.title,
             html: $('<div class="gallery_text">' + item.text + '</div>'), // ...or '<div>123</div>'. Custom HTML inside the frame.
-            fit: 'cover', // Override the global fit option.
             any: 123
         }
     };
--- src/js/partials/gallery.js
+++ src/js/partials/gallery.js
@@ -8,5 +8,40 @@ var gallery = [
         img: 'img/gallery/dddd.jpg',
         title: 'Какая-то башка',
         text: 'Тонкое проникновение в дух лермонтовской поэзии характерно и для других иллюстраций Врубеля. В рисунке к поэме "Измаил-Бей" художник прекрасно передал чувство грусти, вызванное минутой расставания'
+    },
+    {
+        img: 'img/gallery/rain.jpg',
+        title: 'Дождь',
+        text: 'Что ожидать, покупая картины Леонида Афремова?'
+    },
+    {
+        img: 'img/gallery/lisa.jpg',
+        title: 'Мона Лиза',
+        text: 'Кстати, в книге рекордов Гинесса "Мона Лиза" указана, как застрахованная на самую большую сумму картина'
+    },
+    {
+        img: 'img/gallery/eleph.jpg',
+        title: 'Слон',
+        text: ' Название Картины Неизвестно'
+    },
+    {
+        img: 'img/gallery/rain.jpg',
+        title: '',
+        text: ''
+    },
+    {
+        img: 'img/gallery/rain.jpg',
+        title: '',
+        text: ''
+    },
+    {
+        img: 'img/gallery/rain.jpg',
+        title: '',
+        text: ''
+    },
+    {
+        img: 'img/gallery/rain.jpg',
+        title: '',
+        text: ''
     }
 ];
